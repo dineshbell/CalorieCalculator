0337d3b5b804b02e1b6c88080e3b757b98160411
Bellam Dinesh <dineshbellam98@gmail.com>
2023-05-01 10:18:46 +0530

    calorie

--- data_schema.js
+++ data_schema.js
@@ -1,59 +1,45 @@
 const mongoose = require('mongoose');
 
 
-let CalorieIntake = new mongoose.Schema ({
-  //  date: { 
-  //    type: Date, 
-  //    required: true
-  //   },
-  userName: { 
-    type: String, 
-    required: true
-   },
-  totalCaloriesIntake: { 
-    type: Number, 
-    required: true 
+let CalorieCal = new mongoose.Schema ({
+    date: { 
+     type: Date, 
+     default: Date.now
+     },
+    userName: { type: String, require: true},
+    totalCaloriesIntake: { type: Number, required: false },
+    targetCaloriesIntake: {type: Number,required: true },
+    targetAchievedForCaloriesIntake: { type: Boolean, required: false },
+    food: {type: String, required: true},
+    quantity: {type: Number, required: true },
+    totalCaloriesBurned: { type: Number, required: false },
+    targetCaloriesBurned: { type: Number, required: true },
+    targetAchievedForCaloriesBurned: { type: Boolean, required: false },
+    activity: { type: String, required: true },
+    steps: { type: Number, required: true },
   },
-  targetCaloriesIntake: {
-    type: Number,
-     required: true 
-    },
-  targetAchievedForCaloriesIntake: { 
-    type: Boolean, 
-    required: true 
-  },
-  food: {
-     type: String, 
-     required: true
-    },
-  quantity: {
-     type: Number, 
-     required: true 
-    },
-  },
+     {timestamps : true},
+  )
 
-    {timestamps : true},
+//  let CalorieBurned = new mongoose.Schema({
+//   //  date: { type: Date, required: true },
+//    userName: { type: String, required: true },
+//    totalCaloriesBurned: { type: Number, required: true },
+//    targetCaloriesBurned: { type: Number, required: true },
+//    targetAchievedForCaloriesBurned: { type: Boolean, required: true },
+//    activity: { type: String, required: true },
+//    steps: { type: Number, required: true },
 
-)
+// },
 
- const  CalorieBurned = new mongoose.Schema({
-  //  date: { type: Date, required: true },
-   userName: { type: String, required: true },
-   totalCaloriesBurned: { type: Number, required: true },
-   targetCaloriesBurned: { type: Number, required: true },
-   targetAchievedForCaloriesBurned: { type: Boolean, required: true },
-   activity: { type: String, required: true },
-   steps: { type: Number, required: true },
+//   {timestamps : true},
 
-},
-
-  {timestamps : true},
-
-)
+// )
   
   
   
   // define models for the calorie intake and calorie burned records
-  module.exports = mongoose.model('CalorieIntake', CalorieIntake);
-  module.exports = mongoose.model('CalorieBurned', CalorieBurned);
+  // module.exports = mongoose.model('CalorieIntake', CalorieIntake);
+  // module.exports = mongoose.model('CalorieBurned', CalorieBurned);
+  module.exports = mongoose.model('CalorieCal', CalorieCal);
   
--- server.js
+++ server.js
@@ -1,139 +1,165 @@
-const express = require('express')
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const CalorieIntake = require('./data_schema')
-const CalorieBurned = require('./data_schema')
-const cors = require('cors')
+const mongoose = require("mongoose");
+const CalorieCal = require("./data_schema");
+// const CalorieIntake = require('./data_schema')
+// const CalorieBurned = require('./data_schema')
+const cors = require("cors");
 
-
-mongoose.connect('mongodb+srv://dineshbellam:bellam@cluster0.dwfidd1.mongodb.net/?retryWrites=true&w=majority').then(
-    () => console.log('DB Connection established')
-)
+mongoose
+  .connect(
+    "mongodb+srv://dineshbellam:bellam@cluster0.dwfidd1.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("DB Connection established"));
 
 app.use(express.json());
-app.use(cors({origin:"*"}))
+app.use(cors({ origin: "*" }));
 
-
-app.post('/calorie-intake', async(req, res) => {
-  try{  
-    const { date, userName,totalCaloriesIntake,  targetCaloriesIntake, targetAchievedForCaloriesIntake, food, quantity } = req.body;
-    let newcalorieIntake = new CalorieIntake({  date, userName,totalCaloriesIntake,  targetCaloriesIntake, targetAchievedForCaloriesIntake, food, quantity });
-    
-    await newcalorieIntake.save();
-
-    res.status(200).send('Sucessful');
-  }
-  catch (err) {
-    console.log(err)
-    res.status(500).send('Internal server error');
-  }
-}); 
-
-
-app.get('/calorie-intake', async (req, res) => {
+// app.post('/calorie-intake', async(req, res) => {
+app.post("/calorie-cal", async (req, res) => {
   try {
-    const caloriesData = await CalorieIntake.find().sort({date: -1});
-    res.json(caloriesData);
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Server Error');
-  }
-});
-
-app.put('/calorie-intake/:id', async (req, res) => {
-  try {
-    const { id } = req.params;
-    const { totalCaloriesIntake, targetCaloriesIntake, totalCaloriesBurned, targetCaloriesBurned } = req.body;
-    const dailyIntake = await CalorieIntake.findByIdAndUpdate(id, {
+    const {
+       date,
+      userName,
       totalCaloriesIntake,
       targetCaloriesIntake,
+      targetAchievedForCaloriesIntake,
+      food,
+      quantity,
       totalCaloriesBurned,
-      targetCaloriesBurned
-    }, { new: true });
-    res.json(dailyIntake);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Server Error' });
-  }
-});
+      targetCaloriesBurned,
+      targetAchievedForCaloriesBurned,
+      activity,
+      steps,
+    } = req.body;
+    let newcalorieCal = new CalorieCal({
+      date,
+      userName,
+      targetCaloriesIntake,
+      totalCaloriesIntake,
+      targetAchievedForCaloriesIntake,
+      food,
+      quantity,
+      totalCaloriesBurned,
+      targetCaloriesBurned,
+      targetAchievedForCaloriesBurned,
+      activity,
+      steps,
+    });
 
-app.delete('/calorie-intake/:id', async (req, res) => {
-  try {
-    const calorieIntake = await CalorieIntake.findById(req.params.id);
-    if (!calorieIntake) {
-      return res.status(404).json({ message: 'Record not found' });
-    }
-    await calorieIntake.remove();
-    res.json({ message: 'Record deleted' });
+    await newcalorieCal.save();
+
+    res.status(200).send("Sucessful");
   } catch (err) {
-    res.status(500).json({ message: err.message });
+    console.log(err);
+    res.status(500).send("server error");
   }
 });
 
-
-
-// CalorieBurned
-
-
-
-app.post('/calorie-burned', async(req, res) => {
-  try{  
-    const { date, userName,totalCaloriesBurned,  targetCaloriesBurned,  targetAchievedForCaloriesBurned,activity, steps } = req.body;
-    let newcalorieBurned = new CalorieIntake({ date, userName,totalCaloriesBurned,  targetCaloriesBurned,  targetAchievedForCaloriesBurned,activity, steps });
-    
-    await newcalorieBurned.save();
-
-    res.status(200).send('Sucessful');
-  }
-  catch (err) {
-    console.log(err)
-    res.status(500).send('Internal server error');
-  }
-});
-
-
-app.get('/calorie-burned', async (req, res) => {
+app.get("/calorie-cal", async (req, res) => {
   try {
-    const caloriesData = await CalorieBurned.find().sort({date: -1});
+    const caloriesData = await CalorieCal.find().sort({ date: -1 });
     res.json(caloriesData);
   } catch (err) {
     console.error(err.message);
-    res.status(500).send('Server Error');
+    res.status(500).send("Server Error");
   }
 });
 
 
-app.put('/calorie-burned/:id', async (req, res) => {
+app.get("/calorie-cal/:id", async (req, res) => {
   try {
-    const { id } = req.params;
-    const {totalCaloriesBurned, targetCaloriesBurned } = req.body;
-    const dailyIntake = await CalorieIntake.findByIdAndUpdate(id, {
-      totalCaloriesBurned,
-      targetCaloriesBurned
-    }, { new: true });
-    res.json(dailyIntake);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Server Error' });
+    const caloriesData = await CalorieCal.findById(req.params.id);
+    res.json(caloriesData);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
   }
 });
 
+ app.put('/calorie-cal/:id', async (req, res) => {
+   try {
+    const {id} = req.params;
+     const { userName,date,food,quantity,steps,activity,totalCaloriesIntake, 
+      targetCaloriesIntake, totalCaloriesBurned, targetCaloriesBurned } = req.body;
+     const dailyIntake = await CalorieCal.findByIdAndUpdate(id, {
+       totalCaloriesIntake,
+       targetCaloriesIntake,
+       totalCaloriesBurned,
+       targetCaloriesBurned,
+       userName,date,food,quantity,steps,activity
+     });
+     res.json(dailyIntake);
+   } catch (error) {
+     console.error(error);
+     res.status(500).json({ message: 'Server Error' });
+   }
+ });
 
-app.delete('/calorie-intake/:id', async (req, res) => {
+app.delete("/calorie-cal/:id", async (req, res) => {
   try {
-    const calorieBurned = await CalorieIntake.findById(req.params.id);
-    if (!calorieBurned) {
-      return res.status(404).json({ message: 'Record not found' });
-    }
-    await calorieBurned.remove();
-    res.json({ message: 'Record deleted' });
+    const caloriecal = await CalorieCal.findByIdAndDelete(req.params.id);
+    res.json({ message: "Record deleted" });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
 });
 
+// // CalorieBurned
 
+// app.post('/calorie-burned', async(req, res) => {
+//   try{
+//     const { date, userName,totalCaloriesBurned,  targetCaloriesBurned,  targetAchievedForCaloriesBurned,activity, steps } = req.body;
+//     let newcalorieBurned = new CalorieBurned({ date, userName,totalCaloriesBurned,  targetCaloriesBurned,  targetAchievedForCaloriesBurned,activity, steps });
 
-app.listen(3004,()=>{
-    console.log("Server is Running....")
-})
+//     await newcalorieBurned.save();
+
+//     res.status(200).send('Sucessful');
+//   }
+//   catch (err) {
+//     console.log(err)
+//     res.status(500).send('Internal server error');
+//   }
+// });
+
+// app.get('/calorie-burned', async (req, res) => {
+//   try {
+//     const caloriesData = await CalorieBurned.find().sort({date: -1});
+//     res.json(caloriesData);
+//   } catch (err) {
+//     console.error(err.message);
+//     res.status(500).send('Server Error');
+//   }
+// });
+
+// app.put('/calorie-burned/:id', async (req, res) => {
+//   try {
+//     const { id } = req.params;
+//     const {totalCaloriesBurned, targetCaloriesBurned } = req.body;
+//     const dailyIntake = await CalorieIntake.findByIdAndUpdate(id, {
+//       totalCaloriesBurned,
+//       targetCaloriesBurned
+//     }, { new: true });
+//     res.json(dailyIntake);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ message: 'Server Error' });
+//   }
+// });
+
+// app.delete('/calorie-burned/:id', async (req, res) => {
+//   try {
+//     const calorieBurned = await CalorieIntake.findById(req.params.id);
+//     if (!calorieBurned) {
+//       return res.status(404).json({ message: 'Record not found' });
+//     }
+//     await calorieBurned.remove();
+//     res.json({ message: 'Record deleted' });
+//   } catch (err) {
+//     res.status(500).json({ message: err.message });
+//   }
+// });
+
+app.listen(3004, () => {
+  console.log("Server is Running....");
+});
